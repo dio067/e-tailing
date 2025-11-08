54c22f4b4608769122a7980f92f81fedcb271811
dio067 <yousefnas06@gmail.com>
2025-11-08 22:08:01 +0400

    feat(frontend): add root App component with routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-paper">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+      <Toaster position="bottom-right" />
+    </div>
+  );
+}
+export default App;
