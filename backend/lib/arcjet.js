0fd4b02faa19415c60ed93b4cbbc2d50a3b03125
dio067 <yousefnas06@gmail.com>
2025-11-08 23:24:01 +0400

    fix(arcjet): correct search engine bot category name

--- backend/lib/arcjet.js
+++ backend/lib/arcjet.js
@@ -1,26 +1,23 @@
-import arcjet, {tokenBucket, shield, detectBot} from '@arcjet/node';
+import arcjet, { tokenBucket, shield, detectBot } from '@arcjet/node';
 
 import 'dotenv/config';
 
-export default arcjet ({
-    key: process.env.ARCJET_KEY,
-    log: console,
-    characteristics: ["ip.src"],
-    rules: [
-        shield( {mode: 'LIVE'}),
-        detectBot( {mode: 'LIVE',
-        
-            allow: [
-            "CATEGORY:SEARCH_ENGINE_BOT"
-            ]
-
-}),
-        tokenBucket( {
-            mode: 'LIVE',
-            refillRate: 5,
-            interval: 10,
-            capacity: 10 
-        }) 
-
-    ]
-})
+export default arcjet({
+  key: process.env.ARCJET_KEY,
+  log: console,
+  characteristics: ['ip.src'],
+  rules: [
+    shield({ mode: 'LIVE' }),
+    detectBot({
+      mode: 'LIVE',
+      allow: ['CATEGORY:SEARCH_ENGINE'],
+    }),
+    ,
+    tokenBucket({
+      mode: 'LIVE',
+      refillRate: 5,
+      interval: 10,
+      capacity: 10,
+    }),
+  ],
+});
